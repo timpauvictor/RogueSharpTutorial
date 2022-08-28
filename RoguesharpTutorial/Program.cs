b99f7cede74e377976a43cc561a44b6119fbab39
Victor Timpau <timpauvictor@gmail.com>
2022-08-27 22:40:53 -0400

    initial commit

---
+++ RoguesharpTutorial/Program.cs
@@ -0,0 +1,43 @@
+﻿using RLNET;
+ 
+namespace RogueSharpTutorial
+{
+    public class Program
+    {
+        // The screen height and width are in number of tiles
+        private static readonly int _screenWidth = 100;
+        private static readonly int _screenHeight = 70;
+ 
+        private static RLRootConsole _rootConsole;
+ 
+        public static void Main()
+        {
+            // This must be the exact name of the bitmap font file we are using or it will error.
+            string fontFileName = "terminal8x8.png";
+            // The title will appear at the top of the console window
+            string consoleTitle = "RougeSharp V3 Tutorial - Level 1";
+            // Tell RLNet to use the bitmap font that we specified and that each tile is 8 x 8 pixels
+            _rootConsole = new RLRootConsole( fontFileName, _screenWidth, _screenHeight, 
+                8, 8, 1f, consoleTitle );
+            // Set up a handler for RLNET's Update event
+            _rootConsole.Update += OnRootConsoleUpdate;
+            // Set up a handler for RLNET's Render event
+            _rootConsole.Render += OnRootConsoleRender;
+            // Begin RLNET's game loop
+            _rootConsole.Run();
+        }
+ 
+        // Event handler for RLNET's Update event
+        private static void OnRootConsoleUpdate( object sender, UpdateEventArgs e )
+        {
+            _rootConsole.Print( 10, 10, "It worked!", RLColor.White );
+        }
+ 
+        // Event handler for RLNET's Render event
+        private static void OnRootConsoleRender( object sender, UpdateEventArgs e )
+        {
+            // Tell RLNET to draw the console that we set
+            _rootConsole.Draw();
+        }
+    }
+}
